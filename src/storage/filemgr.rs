d57c9d7043c966bd9b7e57ab4c27038cd34d6a1f
Xiang Yu Tuang <xiangyutuang@gmail.com>
2025-04-15 23:29:05 -0400

    read function zzz

--- src/storage/filemgr.rs
+++ src/storage/filemgr.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::sync::{Arc, Mutex, RwLock};
+use std::io::{SeekFrom, Seek, Read};
 use crate::storage::{block::Block, page::Page};
 
 pub struct FileMgr {
@@ -16,16 +17,18 @@ impl FileMgr {
     }
 
     // Reads block into page
-    pub fn read(&mut self, blk: &Block, p: &Page) {
+    pub fn read(&mut self, blk: &Block, p: &mut Page) {
         let file_lock = self.get_file(blk.get_fname());
         let read_guard = file_lock.read().unwrap();
-        let f = &*read_guard;
-        let buffer = p.get_buffer();
-        let offset = (blk.get_blknum() * self.blksize) as u64
+        let mut f = &*read_guard;
+        let mut buffer = &mut p.get_buffer();
+        let offset = (blk.get_blknum() * self.blksize) as u64;
+        let tmp: &mut [u8] = &mut vec![0; buffer.len()]; //a temp buffer to store content from file
         
         buffer.set_wpos(0); // move write pos to the beginning of this page
         f.seek(SeekFrom::Start(offset));
-        f.read(buffer, buffer.size());
+        f.read_exact(tmp);
+        buffer.write_bytes(tmp);
     }
 
     // Writes from page to block
